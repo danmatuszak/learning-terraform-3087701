3136db241bbe2197964b32d6a09bf77af3b2299a
Daniel Matuszak <dmatuszak@ucdavis.edu>
2023-03-16 13:34:03 -0700

    fix naming errors

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform = "true"
@@ -34,7 +35,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog"
 
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
